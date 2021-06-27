3fb97a39f6511b48ed8afd819aebe586f9468682
Todd Krissel <ctkads@yahoo.com>
2021-06-27 01:28:50 -0700

    Add simple tests.

--- src/main/java/org/foo/sample/App.java
+++ src/main/java/org/foo/sample/App.java
@@ -2,12 +2,14 @@ package org.foo.sample;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+  public static boolean checkEquals(Integer valueOne, Integer valueTwo) {
+    return valueOne.equals(valueTwo);
+  }
 }
--- src/test/java/org/foo/sample/AppTest.java
+++ src/test/java/org/foo/sample/AppTest.java
@@ -1,5 +1,6 @@
 package org.foo.sample;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,23 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void shouldBeEqual() {
+    assertTrue(org.foo.sample.App.checkEquals(42, 42));
+  }
+
+  @Test
+  public void shouldBeInequal() {
+    assertFalse(org.foo.sample.App.checkEquals(41, 42));
+  }
 }
